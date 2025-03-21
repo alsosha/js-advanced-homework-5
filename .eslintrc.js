97f0a4a6ca5d0a69960cad1f42bb51dd18f66709
Софья Шандыбина <barsuk2001@gmail.com>
2025-03-21 17:16:49 +0300

    workspace set

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'env': {
+      'browser': true,
+      'es6': true,
+      'node': true,
+      'jest': true
+    },
+    'extends': [
+      'eslint:recommended',
+      'airbnb-base'
+    ],
+    'overrides': [],
+    'parserOptions': {
+      'ecmaVersion': 'latest',
+      'sourceType': 'module'
+    },
+    'rules': {}
+  }
